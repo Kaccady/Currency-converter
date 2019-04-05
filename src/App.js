3c57d172c48824415488be9b299e44a4a14207eb
Kaccady <z-rz@mail.com>
2019-04-05 22:27:03 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import fetchedCurrency from "./json";
 
+const Currency = ({ props: { rates } }) => {
+  for (let name in rates) {
+    console.log(name);
+    return <p>{name}</p>
+  }
+};
 class App extends Component {
+  state = { data: fetchedCurrency };
+
   render() {
+    console.log(this.state.data.rates);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Currency props={this.state.data} />
       </div>
     );
   }
