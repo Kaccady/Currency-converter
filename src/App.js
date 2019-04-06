b11778c1a000b06870c7df8d6e037fa518adf551
Kaccady <z-rz@mail.com>
2019-04-06 17:12:06 +0300

    change

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React, { Component } from "react";
 import fetchedCurrency from "./json";
 
-const Currency = ({ props: { rates } }) => {
-  for (let name in rates) {
-    console.log(name);
-    return <p>{name}</p>
-  }
-};
+/*const Currency = ({ props: { rates } }) => {
+  const CurrencyList = () => {
+    for (let name in rates) {
+      console.log(name, rates[name]);
+      return <p>{"2".toString()}</p>;
+    }
+  };
+  return <div>{ CurrencyList }</div>;
+};*/
+const Currency = ({props:{id,value}}) => 
+  <p>
+    валюта: {id} значение: {value}
+  </p>;
+
+const CurrencyList =({ props: { rates } }) => {
+  for (let name in rates){
+    return(
+    <Currency props={({id:name,value:rates[name]})}/>);}}
+
+
 class App extends Component {
   state = { data: fetchedCurrency };
-
   render() {
-    console.log(this.state.data.rates);
     return (
       <div className="App">
-        <Currency props={this.state.data} />
+        <CurrencyList props={this.state.data} />
+        {CurrencyList}
       </div>
     );
   }
